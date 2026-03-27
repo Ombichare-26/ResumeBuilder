32b9ee18eae73a42064b56d9ef8a6e69a4d0751a
Ombichare@26 <ombichare2006@gmail.com>
2026-03-27 19:16:35 +0530

    Added app.js db.js server.js code

---
+++ Backend/app.js
@@ -0,0 +1,32 @@
+import express from "express";
+import cors from "cors";
+
+
+// Routes
+// import authRoutes from "./routes/authRoutes.js";
+// import resumeRoutes from "./routes/resumeRoutes.js";
+// import matchRoutes from "./routes/matchRoutes.js";
+
+const app = express();
+
+// Middleware
+app.use(cors({
+    origin: process.env.CORS_ORIGIN || "*",
+    credentials: true,
+}));
+
+// app.use(express.json({ limit: "16kb" }));
+// app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+// app.use(cookieParser());
+
+// Routes
+// app.use("/api/auth", authRoutes);
+// app.use("/api/resume", resumeRoutes);
+// app.use("/api/match", matchRoutes);
+
+// Health check
+app.get("/", (req, res) => {
+    res.send("Backend is running");
+});
+
+export default app;
--- Backend/server.js
+++ Backend/server.js
@@ -1,1 +1,21 @@
 //server.js -> Loads .env -> connectDB() -> imports app.js -> (app.js sets routes + middleware) -> server starts listening
+
+import dotenv from "dotenv";
+import app from "./app.js";
+import connectDB from "./config/db.js";
+
+
+dotenv.config({
+    path: "./.env"
+});
+
+connectDB().then(() => {
+    const port = process.env.PORT || 5000;
+    app.listen(port, () => {
+        console.log("app is listening at port:", port);
+    });
+
+})
+    .catch((error) => {
+        console.log("MONGODB CONNECTION ERROR!!:", error);
+    });;
